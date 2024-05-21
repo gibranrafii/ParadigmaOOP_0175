ee492f851388818366fb19cda1efe236436d41d1
gibranrafii <gibranrafi21@gmail.com>
2024-05-21 12:45:21 +0700

    Membuat prosedur tambahPasien

--- Praktikum 10.4 (asosiasi)/Praktikum 10.4 (asosiasi).cpp
+++ Praktikum 10.4 (asosiasi)/Praktikum 10.4 (asosiasi).cpp
@@ -45,6 +45,11 @@ void pasien::cetakDokter() {
     cout << endl;
 }
 
+void dokter::tambahPasien(pasien* pPasien) {
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+
 int main()
 {
     
